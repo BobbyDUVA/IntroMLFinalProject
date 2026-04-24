9e4526e183eec26fd2814b21bd5d2c4f5ba51d0f
zachary-t-denison <tsx4wu@virginia.edu>
2026-04-24 15:16:36 -0400

    Linear Regression Done

--- movies.py
+++ movies.py
@@ -83,25 +83,15 @@ newMovieData["rating_count"] = m["rating_count"]
 newMovieData["rating"] = m["rating"]
 
 
-X = newMovieData.drop(["title", "rating"], axis = 1) # remove label and target variable from X
-y = newMovieData["rating"]
-
-
-# Applying the NaN checker 
-#count_nan = newMovieData['release_date'].isnull().sum()
-# printing the number of values present
-# in the column
-#print('Number of NaN values present: ' + str(count_nan))
-
-
-# drop data with Nan
 newMovieData.dropna(how="any", inplace=True)
 print(len(newMovieData)) # 634 movies
 
 
-# Section: Method 1 Implementation (Feature Scaling) --- Bobby
+X = newMovieData.drop(["title", "rating"], axis = 1).to_numpy() # remove label and target variable from X
+y = newMovieData["rating"].to_numpy()
 
-from sklearn.model_selection import train_test_split
+
+# Section: Method 1 Implementation (Feature Scaling) --- Bobby
 
 def train_test_split(X, y, test_size = 0.2, random_state = 42):  # Source Notebook 3: 1.3, direct implementation of given code
     np.random.seed(random_state)
@@ -119,5 +109,50 @@ def train_test_split(X, y, test_size = 0.2, random_state = 42):  # Source Notebo
 X_train, X_test, y_train, y_test = train_test_split(X, y)
 print(f"Train: {len(y_train)}, Test: {len(y_test)}")
 
+def normalize_zscore(X_train, X_test): #Source Notebook 3: 1.4, direct implementation of given code again
+    mean = np.mean(X_train, axis=0)
+    std = np.std(X_train, axis=0)
+    
+    X_train_norm = (X_train - mean) / std
+    X_test_norm = (X_test - mean) / std
+    return X_train_norm, X_test_norm
 
+X_train_norm, X_test_norm = normalize_zscore(X_train, X_test)
+print(f"Normalized mean: {np.mean(X_train_norm, axis=0).round(4)}")
+print(f"Normalized std:  {np.std(X_train_norm, axis=0).round(4)}")
+
+def knn_predict_regression(X_train, y_train, X_test, k=3): #Source Notebook 3: 1.5
+    #kNN Pred using kNN - Regression, almost identical to what was given in class, just minor changes that are explained
+    # https://www.geeksforgeeks.org/machine-learning/k-nearest-neighbors-knn-regression-with-scikit-learn/
+    predictions = []
+    
+    for x_test in X_test:
+        distances = np.linalg.norm(X_train - x_test, axis=1)
+        
+        k_nearest_indices = np.argsort(distances)[:k]
+        k_nearest_labels = y_train[k_nearest_indices]
+        
+        pred = np.average(k_nearest_labels)
+        # I just averaged instead of doing a count comparison, otherwise the code is identical to that provided in the notebook.
+        predictions.append(pred)
+    
+    return np.array(predictions)
+
+def compute_metrics(y_true, y_pred): #Idea taken directly from Notebook 3 again, section 1.6
+    rmse = np.sqrt(np.mean((y_true - y_pred) ** 2))
+    #I just added RMSE for a comparison since you know it's, 
+    #regression instead of classification this time, different ball game.
+    #I would add accuracy, recall, precision, but as you are well aware that only works 
+    #for classification problems.
+    return rmse
+
+y_pred = knn_predict_regression(X_train, y_train, X_test, k=5)
+rmse_non_normalized = compute_metrics(y_test, y_pred)
+print("RMSE NON NORMALIZED: " + str(rmse_non_normalized))
+
+y_pred_norm = knn_predict_regression(X_train_norm, y_train, X_test_norm, k=5)
+rmse_normalized = compute_metrics(y_test, y_pred_norm)
+print("RMSE NORMALIZED: " + str(rmse_normalized))
+
+from sklearn.linear_model import LinearRegression
 
